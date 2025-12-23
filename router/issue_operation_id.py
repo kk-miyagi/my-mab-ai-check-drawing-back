c4fa3a3ef3810792eb4684ccaa465d897026c3eb
asunada.akihiko.66d <sunada.akihiko.66d@jp.nssol.nipponsteel.com>
2025-12-23 15:48:00 +0900

    URL '_' to '-'2

--- router/issue_operation_id.py
+++ router/issue_operation_id.py
@@ -1,10 +1,11 @@
 from app_router import AppRouter
-from fastapi import Request 
+from fastapi import Request
 from manager.app_status_manager import AppStatus
 
 router = AppRouter()
 
-@router.post('/issue/operation_id/')
+
+@router.post('/issue/operation-id/')
 async def issue_operation_id(request: Request):
     req_status = AppStatus.create_from_request(request.state.body)
     session_status = AppStatus.create_app_session(
@@ -14,4 +15,3 @@ async def issue_operation_id(request: Request):
     return router.create_responce_from_status(
             session_status
     )
-
