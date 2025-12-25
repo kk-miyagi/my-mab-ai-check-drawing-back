3ec40a8289c5fa4ca18a31f81efbdbb2a4c2ffeb
Ogawa Norihiro <ogawa.norihiro.8nk@jp.nssol.nipponsteel.com>
2025-12-25 17:40:24 +0900

    MAB_GAI_POC-154 【実装】バッチ非同期起動とID更新

---
+++ state_func/boot_another_process_func.py
@@ -0,0 +1,39 @@
+from state.boot_another_process_info import BaseBootAnotherProcessInfo
+
+
+def create_boot_process_info(self):
+    with self.lock:
+        if not hasattr(
+            self.app_state,
+            "BOOT_ANOTHER_PROCESS_SESSION_KEY"
+        ):
+            self.app_state.BOOT_ANOTHER_PROCESS_SESSION_KEY = {}
+
+
+def get_boot_process_info(self, req_status):
+    with self.lock:
+        ret = self.app_state.BOOT_ANOTHER_PROCESS_SESSION_KEY[
+            req_status.get_hash_key()
+        ]
+    return ret
+
+
+def get_session_dict(self):
+    with self.lock:
+        ret = self.app_state.BOOT_ANOTHER_PROCESS_SESSION_KEY
+    return ret
+
+
+def update_boot_process_info(self, status):
+    session_dic = self.app_state.BOOT_ANOTHER_PROCESS_SESSION_KEY
+    if status.get_hash_key() not in session_dic:
+        session_dic[status.get_hash_key()] = BaseBootAnotherProcessInfo(
+            status.user,
+            status.epic,
+            status.operation,
+            status.operation_id,
+            status.status
+        )
+    else:
+        loader = session_dic[status.get_hash_key()]
+        loader.status = status.status
