7e744188382afb8808a9f469afc5aee526bae637
Ogawa Norihiro <ogawa.norihiro.8nk@jp.nssol.nipponsteel.com>
2026-01-29 16:37:03 +0900

    update getRedirectUrl

--- mock-job-demo/src/routers/getRedirectUrl.ts
+++ mock-job-demo/src/routers/getRedirectUrl.ts
@@ -4,10 +4,10 @@ export const getRedirectUrl = (raw: string | null): string | undefined => {
   if (!raw) return undefined;
   try {
     const parsed = JSON.parse(raw) as PersistedState;
-    if (!parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'multi-file-upload' && parsed.status === 'doing') return '/create-label-processing'
-    if (!parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'batch-create-label' && parsed.status === 'doing') return '/create-label-processing'
+    if (!parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'batch-create-label' && parsed.status === 'start') return '/create-label-processing'
     if (!parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'batch-create-label' && parsed.status === 'end') return '/create-label-result'
-    if (parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'multi-file-upload' && parsed.status === 'doing') return '/demo-create-label-processing'
+    if (!parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'batch-create-label' && parsed.status === 'error') return '/create-label'
+    if (!parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'open-update-label-screen' && parsed.status === 'start') return '/update-label'
     if (parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'batch-create-label' && parsed.status === 'doing') return '/demo-create-label-processing'
     if (parsed.demoFlag && parsed.lastEpic === 'create-label' && parsed.lastOperation === 'batch-create-label' && parsed.status === 'end') return '/demo-create-label-result'
   } catch {
