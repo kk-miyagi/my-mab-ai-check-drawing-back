97b845286aaf834a3dd094d600eb8a7e83c4e139
Ogawa Norihiro <ogawa.norihiro.8nk@jp.nssol.nipponsteel.com>
2026-03-04 17:36:36 +0900

    demo response

--- mock-job-demo/src/api/drawingCompareApi.ts
+++ mock-job-demo/src/api/drawingCompareApi.ts
@@ -32,7 +32,43 @@ export const drawingCompareApi = {
     form.append('operation', payload.operation);
     form.append('operation_id', payload.operation_id);
     form.append('status', payload.status);
-    return postForm(GET_IMAGE_SIMILARITY, form)
+    // return postForm(GET_IMAGE_SIMILARITY, form)
+    return {
+      user: payload.user,
+      epic: payload.epic,
+      operation: payload.operation,
+      operation_id: payload.operation_id,
+      status: 'end',
+
+      base_rects: {
+        "bf_1": [497, 123, 412, 421],
+        "bf_2": [275, 682, 432, 425],
+        "bf_3": [68, 57, 410, 451],
+      },
+      target_rects: {
+        "af_1": [497, 123, 412, 421],
+        "af_2": [275, 682, 432, 425],
+        "af_3": [68, 57, 410, 451]
+      },
+      similarities: {
+        "bf_1": {
+          "af_1": 10,
+          "af_2": 20,
+          "af_3": 30,
+        },
+        "bf_2": {
+          "af_1": 40,
+          "af_2": 50,
+          "af_3": 60,
+        },
+        "bf_3": {
+          "af_1": 70,
+          "af_2": 80,
+          "af_3": 90,
+        },
+      }
+    };
+
   },
 
 
