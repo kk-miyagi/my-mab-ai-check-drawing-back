ce33f0ca259c91e989c8390fc284451fc660815a
Ogawa Norihiro <ogawa.norihiro.8nk@jp.nssol.nipponsteel.com>
2026-03-12 16:26:22 +0900

    modify

--- backend_tasks/drawing_compare_task.py
+++ backend_tasks/drawing_compare_task.py
@@ -7,6 +7,7 @@ import re
 from pathlib import Path
 from vertexai.generative_models import GenerativeModel
 import io
+from datetime import datetime
 from utils.simple_multi_genemipronpt import (
         generate_with_multiple_contents,
 )
@@ -14,27 +15,26 @@ from utils.gemini_response import (
     get_raw_response,
 )
 
-
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'poc-shared-mab-ai-adv-util-sa.json'
-
 model = GenerativeModel("gemini-2.5-pro")
 
-def check_base_draw_list(base_draw_list, compare_list):
+def check_draw_list(draw_list, compare_list, draw_kind, out_dir):
+    """一覧と比較結果を確認"""
     prompt = f"""
     客先図面と自社用図面から設計情報を取得して、差分比較を行いました。
 
-    客先図面の特徴一覧情報
-    {base_draw_list}
+    {draw_kind}図面の特徴一覧情報
+    {draw_list}
 
     客先図面と自社用図面を比較した結果
     {compare_list}
 
     ルール
-    1:比較した結果をもとに、客先の一覧に情報があるかないかを確認してください。
-    2:比較結果の「項目」と「客先図面の記載内容」をもとに、客先の一覧にあるかを総合的に判断してください。
-    3:比較結果の「客先図面の記載内容」が「-」となっていれば、そこは不要です。
-    4:表のカラムは、"図面種類","項目","客先図面の記載内容","判定結果","判定理由"の5項目で抽出してください。
-        図面種類：客先
+    1:比較した結果をもとに、{draw_kind}の一覧に情報があるかないかを確認してください。
+    2:比較結果の「項目」と「{draw_kind}図面の記載内容」をもとに、{draw_kind}の一覧にあるかを総合的に判断してください。
+    3:比較結果の「{draw_kind}図面の記載内容」が「-」となっていれば、そこは不要です。
+    4:表のカラムは、"図面種類","項目","{draw_kind}図面の記載内容","判定結果","判定理由"の5項目で抽出してください。
+        図面種類：{draw_kind}
         判定結果：存在するか場合は「有」。存在しない場合は「無」
     5:文字が完全一致している必要はなく、記載がほぼ同じと総合的に判断できるようだったら判定結果を「有」としてください。
     6:CSV形式で出力してください。値はダブルクォーテーションで囲ってください。
@@ -45,35 +45,22 @@ def check_base_draw_list(base_draw_list, compare_list):
         prompts=[prompt],
     )
     res = get_raw_response(gemini_res)
-    return res
 
-def check_target_draw_list(target_draw_list, compare_list):
-    prompt = f"""
-    客先図面と自社用図面から設計情報を取得して、差分比較を行いました。
+    # 保存
+    if draw_kind == "客先":
+        kind = "base"
+    elif draw_kind == "自社用":
+        kind = "target"
+    save_gemini_responce(res, f"{out_dir}/llm_check_{kind}_draw_list.csv")
+    
+    # 客先のチェック処理の結果を一覧に反映
+    res_reader = list(csv.reader(io.StringIO(res)))
+    draw_list_reader = list(csv.reader(io.StringIO(draw_list)))
+    filtered_reader = [row[1:3] + ["図面比較時に追加"] for row in res_reader[1:] if row[3] == "無"]
+    merged_reader = draw_list_reader + filtered_reader
+    save_gemini_responce(merged_reader, f"{out_dir}/llm_new_{kind}_draw_list.csv")
 
-    自社用図面の特徴一覧情報
-    {target_draw_list}
-
-    客先図面と自社用図面を比較した結果
-    {compare_list}
-
-    ルール
-    1:比較した結果をもとに、自社用の一覧に情報があるかないかを確認してください。
-    2:比較結果の「項目」と「自社用図面の記載内容」をもとに、自社用の一覧にあるかを総合的に判断してください。
-    3:比較結果の「自社用図面の記載内容」が「-」となっていれば、そこは不要です。
-    4:表のカラムは、"図面種類","項目","社内用図面の記載内容","判定結果","判定理由"の5項目で抽出してください。
-        図面種類：自社用
-        判定結果：存在するか場合は「有」。存在しない場合は「無」
-    5:文字が完全一致している必要はなく、記載がほぼ同じと総合的に判断できるようだったら判定結果を「有」としてください。
-    6:CSV形式で出力してください。値はダブルクォーテーションで囲ってください。
-    7:CSV形式以外出力しないでください。
-    """
-    gemini_res = generate_with_multiple_contents(
-        model,
-        prompts=[prompt],
-    )
-    res = get_raw_response(gemini_res)
-    return res
+    return None
 
 def get_draw_list(image):
     """画像から情報を取得して、csv形式に変換する"""
@@ -167,6 +154,27 @@ def get_drawing_compare(base_image_path: str, target_image_paths: list):
     res: str = get_raw_response(res)
     return res
 
+def save_gemini_responce(save_data, save_path, encoding="utf-8-sig"):
+    if type(save_data) is list:
+        reader = save_data.copy()
+    else:
+        reader = csv.reader(io.StringIO(save_data))
+    with open(save_path, "w", encoding=encoding, newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL, doublequote=True)
+        writer.writerows(reader)
+    print(f"save: {save_path}")
+    return None
+
+def change_output(res, base_position, target_position):
+    reader = csv.DictReader(io.StringIO(res, newline=""))
+    rows = list(reader)
+    for r in rows:
+        if r["客先図面の記載位置"] != "-":
+            r["客先図面の記載位置"] = f"全体：「{base_position}」、詳細：「{r["客先図面の記載位置"]}」"
+        if r["社内用図面の記載位置"] != "-":
+            r["社内用図面の記載位置"] = f"全体：「{target_position}」、詳細：「{r["社内用図面の記載位置"]}」"
+    return rows
+
 if __name__ == "__main__":
     start = time.time()
     parse = argparse.ArgumentParser("図面比較")
@@ -202,106 +210,50 @@ if __name__ == "__main__":
     target_cut_dir: str = args.target_cut_dir
     out_dir: dict = args.out_dir
 
+    # 組み合わせ情報を読み込み
     combinations_json_path = f"{out_dir}/combinations.json"
     with open(combinations_json_path, 'r', encoding='utf-8') as f:
         combinations: dict = json.load(f)
 
-    save_cols = ["項目","客先図面の記載内容","客先図面の記載位置","社内用図面の記載内容","社内用図面の記載位置","差分内容","判定結果","判定理由"]
+    run_timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    run_dir = f"{out_dir}/job_{run_timestamp}"
+    Path(run_dir).mkdir(parents=True, exist_ok=True)
 
+    # 図面から一覧を作成
     base_draw_list = get_draw_list(base_image_path)
+    save_gemini_responce(base_draw_list, f"{run_dir}/llm_base_draw_list.csv")
     target_draw_list = get_draw_list(target_image_path)
+    save_gemini_responce(target_draw_list, f"{run_dir}/llm_target_draw_list.csv")
 
-    reader = csv.reader(io.StringIO(base_draw_list))
-    with open(f"{out_dir}/llm_base_draw_list.csv", "w", encoding="utf-8-sig", newline="") as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL, doublequote=True)
-        writer.writerows(reader)
-    reader = csv.reader(io.StringIO(target_draw_list))
-    with open(f"{out_dir}/llm_target_draw_list.csv", "w", encoding="utf-8-sig", newline="") as f:
-        writer = csv.writer(f, quoting=csv.QUOTE_ALL, doublequote=True)
-        writer.writerows(reader)
-
+    # 組み合わせでループ
+    save_cols = ["項目","客先図面の記載内容","客先図面の記載位置","社内用図面の記載内容","社内用図面の記載位置","差分内容","判定結果","判定理由"]
     all_rows = []
     for base, targets in combinations.items():
+        # base: base_1とかtargetsはリスト状態
         target_paths = [f"{target_cut_dir}/{t}.jpg" for t in targets]
         base_position = get_image_position(base_image_path, f"{base_cut_dir}/{base}.jpg")
-        print(f"base_position: {base_position}")
+
+        # targetごとに処理
         for target in targets:
             target_path = f"{target_cut_dir}/{target}.jpg"
-
-            res = get_drawing_compare(
-                f"{base_cut_dir}/{base}.jpg",
-                target_path
-            )
-            print(res)
+            # 比較処理
+            res = get_drawing_compare(f"{base_cut_dir}/{base}.jpg", target_path)
             target_position = get_image_position(target_image_path, target_path)
-            print(f"target_position: {target_position}")
+            rows = change_output(res, base_position, target_position)
+            all_rows.extend(rows)
 
-            input_io = io.StringIO(res, newline="")
-            reader = csv.DictReader(input_io)
-            rows = list(reader)
-
-            up_base_file_name = Path(base_image_path).stem
-            up_base_file_name = re.search(r"\d+_bf_file_(.*)", up_base_file_name).group(1)
-            up_target_file_name = Path(target_image_path).stem
-            up_target_file_name = re.search(r"\d+_bf_file_(.*)", up_target_file_name).group(1)    
-            
-            for r in rows:
-                if r["客先図面の記載位置"] != "-":
-                    r["客先図面の記載位置"] = f"全体：「{base_position}」、詳細：「{r["客先図面の記載位置"]}」"
-                if r["社内用図面の記載位置"] != "-":
-                    r["社内用図面の記載位置"] = f"全体：「{target_position}」、詳細：「{r["社内用図面の記載位置"]}」"
-                # 内部的に持っていてもいいかも？
-                # r["客先図面のID"] = base
-                # r["社内用図面のID"] = target
-            
-            all_rows.extend(rows)  
-            print("-" * 5)
-
-        print(all_rows)
-
-        # チェック処理
-        print("<--------------------------------------------------------------------->")
-        res = check_base_draw_list(base_draw_list, all_rows)
-        print(res)
-        reader = csv.reader(io.StringIO(res))
-        with open(f"{out_dir}/llm_check_base_draw_list.csv", "w", encoding="utf-8-sig", newline="") as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL, doublequote=True)
-            writer.writerows(reader)
-        # チェックの結果を一覧に反映
-        reader = list(csv.reader(io.StringIO(res)))
-        base_draw_list_reader = list(csv.reader(io.StringIO(base_draw_list)))
-        filtered_reader = [row[1:3] + ["図面比較時に追加"] for row in reader[1:] if row[3] == "無"]
-        print(reader)
-        print(f"抽出: {filtered_reader}")
-        merged_reader = base_draw_list_reader + filtered_reader
-        with open(f"{out_dir}/llm_new_base_draw_list.csv", "w", encoding="utf-8-sig", newline="") as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL, doublequote=True)
-            writer.writerows(merged_reader)
-
-        print("---")
-        res = check_target_draw_list(target_draw_list, all_rows)
-        print(res)
-        reader = csv.reader(io.StringIO(res))
-        with open(f"{out_dir}/llm_check_target_draw_list.csv", "w", encoding="utf-8-sig", newline="") as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL, doublequote=True)
-            writer.writerows(reader)
-
-        # チェックの結果を一覧に反映
-        reader = list(csv.reader(io.StringIO(res)))
-        target_draw_list_reader = list(csv.reader(io.StringIO(target_draw_list)))
-        filtered_reader = [row[1:3] + ["図面比較時に追加"] for row in reader[1:] if row[3] == "無"]
-        merged_reader = target_draw_list_reader + filtered_reader
-        print(reader)
-        print(f"抽出: {filtered_reader}")
-        with open(f"{out_dir}/llm_new_target_draw_list.csv", "w", encoding="utf-8-sig", newline="") as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL, doublequote=True)
-            writer.writerows(merged_reader)
-
-
+        up_base_file_name = Path(base_image_path).stem
+        up_base_file_name = re.search(r"\d+_bf_file_(.*)", up_base_file_name).group(1)
+        up_target_file_name = Path(target_image_path).stem
+        up_target_file_name = re.search(r"\d+_bf_file_(.*)", up_target_file_name).group(1)
         with open(f"{out_dir}/{up_base_file_name}_and_{up_target_file_name}_llm_final.csv", "w", newline="", encoding="utf-8-sig") as f:
             writer = csv.DictWriter(f, fieldnames=save_cols, quoting=csv.QUOTE_ALL)
             writer.writeheader()
             writer.writerows(all_rows)
+
+        # 一覧と比較結果のチェック処理
+        check_draw_list(base_draw_list, all_rows, "客先", run_dir)
+        check_draw_list(target_draw_list, all_rows, "自社用", run_dir)
 
     end = time.time()
     print(end - start)
